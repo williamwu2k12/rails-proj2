529630b49e63b91826f276b283c7dbfdc33731c5
Nic Stone <n.stone@berkeley.edu>
2014-12-02 23:55:50 -0800

    got rid of unnecessary patch requests

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   resources :comments
   post '/cats/:id/new', to: 'cats#new', as: :new
   patch 'cats/:id/show', to: 'cats#show', as: :show
-  patch '/comments/upvote', to: 'comments#upvote', as: :upvote
-  patch '/comments/downvote', to: 'comments#update', as: :downvote
 end
