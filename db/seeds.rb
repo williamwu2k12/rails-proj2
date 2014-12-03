20a8825f148e63c1b9cc6f92a6ad5fe0bdb5c264
William Wu <williamwu.2k12@gmail.com>
2014-12-02 21:49:43 -0800

    seeds file updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Cat.delete_all
+Comment.delete_all
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-%w(UCB UCSD UCSB UCI UCLA).each do |name|
-  Cat.create title: name, likes: 10, tag: 'YOLO'
+# Make other users
+%w(WilliamWu NicStone WarrenTian).each do |name|
+  User.create name: name, email: name+"@gmail.com", password: "password"
 end
 
-# Make other users
-%w(Ash Gary Misty Brock).each do |name|
-  User.create name: name, email: name+"@catpics.com", password: 'password'
-end
+Cat.create title: "Siamese", likes: 100, tag: "cute", image: File.open("#{Rails.root}/app/assets/images/Siamese.jpg")
+Cat.create title: "Persian", likes: 100, tag: "beautiful", image: File.open("#{Rails.root}/app/assets/images/Persian.jpg")
+Cat.create title: "Bengal", likes: 100, tag: "disgusting", image: File.open("#{Rails.root}/app/assets/images/Bengal.jpg")
+Cat.create title: "Burmese", likes: 100, tag: "terrifying", image: File.open("#{Rails.root}/app/assets/images/Burmese.jpg")
+Cat.create title: "Tabby", likes: 100, tag: "ugly", image: File.open("#{Rails.root}/app/assets/images/Tabby.jpg")
+Cat.create title: "Siberian", likes: 100, tag: "obese", image: File.open("#{Rails.root}/app/assets/images/Siberian.jpg")
+Cat.create title: "Manx", likes: 100, tag: "sexy", image: File.open("#{Rails.root}/app/assets/images/Manx.jpg")
+
+Comment.create userid: User.where(:name => "WilliamWu").first.id catid: Cat.where(:title => "Siamese").first.id content: "I like big butts and I can not lie, you other brothers cant deny" likes: 10000
+Comment.create userid: User.where(:name => "NicStone").first.id catid: Cat.where(:title => "Siamese").first.id content: "That when a girl walks in with an itty bitty waist" likes: -3
+Comment.create userid: User.where(:name => "WarrenTian").first.id catid: Cat.where(:title => "Siamese").first.id content: "And a round thing in your face" likes: 0
+Comment.create userid: User.where(:name => "WilliamWu").first.id catid: Cat.where(:title => "Siamese").first.id content: "Stop. Hammer time." likes: 69
