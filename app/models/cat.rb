d153f581056754bdd56037a5482dd3f2c8b060e6
William Wu <williamwu.2k12@gmail.com>
2014-12-02 19:49:26 -0800

    created comment model, changed associations between cats, comments, and users

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,5 @@
 class Cat < ActiveRecord::Base
+    belongs_to :user
+    has_many :comments
 	has_attached_file :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203030024) do
+ActiveRecord::Schema.define(version: 20141203034311) do
 
   create_table "cats", force: true do |t|
     t.integer  "user_id"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20141203030024) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.integer  "userid"
+    t.text     "content"
+    t.integer  "likes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
