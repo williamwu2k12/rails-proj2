29aff311d909b66ecbd225587c60f6795b2c2ab4
William Wu <williamwu.2k12@gmail.com>
2014-12-03 23:00:11 -0800

    validation to make sure the form is actually filled out

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,4 +4,7 @@ class Cat < ActiveRecord::Base
 	has_attached_file :image, :attachment_presence => true
 	validates_attachment_content_type :image, :content_type => /\Aimage/
 	validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+    validates :title, presence: true
+    validates :tag, presence: true
+    validates :image, presence: true
 end
