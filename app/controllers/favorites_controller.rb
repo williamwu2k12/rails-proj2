004e974cccec306691db92596ae85e12950c6d10
Nic Stone <n.stone@berkeley.edu>
2014-12-03 16:36:25 -0800

    favorites are now unique, navbar updated

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,21 @@
 class FavoritesController < ApplicationController
 	def update
 		@cat = Cat.find(params["id"])
-		if (params["instruction"] == "Favorite")
-			@favorite = Favorites.new
-        	@favorite.userid = current_user.id
-       		@favorite.catid = @cat.id
-        	@favorite.save
-        	redirect_to :back
-        end
+		Favorites.all.each do |favorite|
+                if (favorite.userid == current_user.id)
+                    if (favorite.catid == @cat.id)
+                        @temp = 1
+                    end
+                end
+            end
+        if (@temp != 1)
+			if (params["instruction"] == "Favorite")
+				@favorite = Favorites.new
+	        	@favorite.userid = current_user.id
+	       		@favorite.catid = @cat.id
+	        	@favorite.save
+	        end
+	    end
+	    redirect_to :back
 	end
 end
